0c52c012ba8e6798b8a4a0b19567dd96d133c7cb
Laurian <laurian@gridinoc.name>
2009-12-29 01:13:15 +0000

    css things

--- script/core.js
+++ script/core.js
@@ -18,9 +18,12 @@ var M = (function() {
 	return {
 		title:	"MUPPLE",
 		base:	"http://github.com/Laurian/MUPPLE/raw/master/",
+		
 		run:	function() {
 			with (jetpack) {
 				future.import("slideBar");
+				future.import("menu");
+				
 				slideBar.append({
 					html:	<html	xmlns="http://www.w3.org/1999/xhtml"
 					      			xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
@@ -40,43 +43,81 @@ var M = (function() {
 									]]></script>
 									<link href="style/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
 									<style type="text/css"><![CDATA[
-										h1	{
-											color:	red !important;
+										#templates	{
+											display:	none;
 										}
 									]]></style>
 								</head>
-								<body class="bp">
+								<body class="slide">
 									<div class="container">
-										<h1 onclick="init();">{M.title}</h1>
+									</div>
+									<div id="templates">
+										<div id="tab">
+											<div class="tab">
+												<h4 class="title"></h4>
+											   	<ul>
+											    	<li>action one</li>
+											        <li>action two</li>
+											        <li>action three</li>
+											    </ul>
+											</div>
+										</div>
 									</div>
 								</body>
 							</html>,
-					width:	400,
+					
+					width:	320,
+					
 					onSelect:	function(slide) { 
-						slide.slide(400, true);
+						slide.slide(320, true);
 					},
+					
 					onReady:	function(slide) {
-						M.loadLibs(slide.contentDocument, function() {
+						var document = slide.contentDocument;
+						
+						M.loadLibs(document, function() {
 							console.log("loaded");
 						});
+						
+						// "import" PURE
+						$.get(M.base + "script/lib/pure_packed.js", function(data, status) {
+							eval(data);
+							//$("p.foo", document).autoRender({foo: "loaded!"});
+						});
+						
 						/*$("h1", slide.contentDocument).bind("click", function() {
 							console.log("click");
-						});
-						//
+						});*/
+						
+						
 						jetpack.tabs.onOpen(function(tab) {
 							
 						});
+						
 						jetpack.tabs.onReady(function(tab) {
-							console.log(tab.label);
-						});*/
-						console.log($("html", slide.contentDocument).html());
-						$.get(M.base + "script/lib/jquery-1.3.2.min.js", function(data, status) {
-							console.log(data);
+							console.log($("#tab", document).html());
+							
+							var t = $($("#tab", document).html(), document);
+							$("div.container", document).append(t);
+							t.autoRender({
+								title: $("title", tab).text()
+							});
+							
 						});
 					}
 				});
+				
+				menu.context.page.add({  
+					label: "MUPPLE",  
+					icon: "http://example.com/ice-cream.png",  
+					menu: new jetpack.Menu(["Record", "Chocolate", "Pistachio", null, "None"]), 
+					command: function(menuitem) {
+						jetpack.notifications.show(menuitem.label)
+					}  
+				});
 			}
 		},
+		
 		loadLibs:	function(document, callback) {
 			//TODO: detect and reuse existing jQuery, see http://jetpackgallery.mozillalabs.com/jetpacks/154
 			with (M) {
@@ -89,18 +130,20 @@ var M = (function() {
 				});
 			}
 		},
+		
 		loadScript:	function(src, document, callback) {
 			var script = document.createElementNS("http://www.w3.org/1999/xhtml", "script");
 			script.src = src;
 			$(script).bind("load", callback);
 			document.getElementsByTagName("head")[0].appendChild(script);
 		},
+		
 		loadStyle:	function(href, document, callback) {
 			var style = document.createElementNS("http://www.w3.org/1999/xhtml", "link");
-			style.type = "text/css";
-			style.rel = "stylesheet";
-			style.href = href;
-			style.media = "screen";
+			style.type 		= "text/css";
+			style.rel 		= "stylesheet";
+			style.href 		= href;
+			style.media 	= "screen";
 			$(style).bind("load", callback);
 			document.getElementsByTagName("head")[0].appendChild(style);
 		}
