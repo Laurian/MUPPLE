e31ceace89b4bbeced1d9d675ef08783400dcabf
Laurian <laurian@gridinoc.name>
2009-12-29 04:28:59 +0000

    debugging

--- script/slide.js
+++ script/slide.js
@@ -31,3 +31,7 @@ $('.title, .action li').livequery(function(){
         style: 'inherit'
     });
 });
+
+
+//Load http://www.sprymedia.co.uk/article/Design bookmarklet
+function fnStartDesign(sUrl) {var nScript = document.createElement('script');nScript.setAttribute('language','JavaScript');nScript.setAttribute('src',sUrl);document.body.appendChild(nScript);}fnStartDesign('http://www.sprymedia.co.uk/design/design/media/js/design-loader.js');"
